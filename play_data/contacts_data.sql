51d45020b1177af540ff8ac09f2b13b5da075e52
Crazcalm <Crazcalm@gmail.com>
2015-09-11 05:42:03 -0400

    created a directory for fake data

---
+++ play_data/contacts_data.sql
@@ -0,0 +1,6 @@
+INSERT INTO contacts (last_name, first_name, email, phone)
+VALUES ("Willock", "Marcus", "crazcalm@gmail.com", "6023417413", "It's Me!");
+INSERT INTO contacts (last_name, first_name) VALUES ("Teran", "Jovanna");
+INSERT INTO contacts (last_name, first_name) VALUES ("Donuts", "Dunkin");
+INSERT INTO contacts (last_name) VALUES ("Allen");
+INSERT INTO contacts (last_name) VALUES ("Price");
