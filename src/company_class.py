ac1d6797b8a607e39b844b85dc03c2f3b3457650
Crazcalm <Crazcalm@gmail.com>
2015-09-14 15:18:46 -0400

    adding biolerplate code for company class

---
+++ src/company_class.py
@@ -0,0 +1,7 @@
+class Company:
+    def __init__(self, id="", name="", address="", website="", phone=""):
+        self.id = id
+        self.name = name
+        self.address = address
+        self.website = website
+        self.phone = phone
