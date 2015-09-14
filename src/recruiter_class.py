0c9b75b3ef977640ab65c6045f7bc8a7d7c78c19
Crazcalm <Crazcalm@gmail.com>
2015-09-14 15:29:57 -0400

    biolerplate for recruiter class

---
+++ src/recruiter_class.py
@@ -0,0 +1,10 @@
+class Recruiter:
+    def __init__(self, uid="", first_name="", last_name="", email="", phone="", description="",
+                 company_uid=None):
+        self.uid = uid
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.phone = phone
+        self.description = description
+        self.company_uid = company_id
