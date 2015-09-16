5b03bb60e59d949eebc4f8310739ce9833bebeb0
Crazcalm <Crazcalm@gmail.com>
2015-09-16 17:41:30 -0400

    I have converted the sql to python strings. I will rewrite the sql class after my gf's birthday

--- src/constants.py
+++ src/constants.py
@@ -97,32 +97,27 @@ CONTACTS_DATA = [
     'INSERT INTO contacts (last_name) VALUES ("Price");'
 ]
 
-RECRUITERS_DATA = []
+RECRUITERS_DATA = [
+    ('INSERT INTO recruiters (first_name, last_name, email, phone, companyId) VALUES ("Recuiter1",'
+     ' "1Recruiter", "recruiter1@gmail.com", "111-111-1111", 1);'),
+    ('INSERT INTO recruiters (first_name, last_name, email, phone, companyId) VALUES ("Recuiter2",'
+     ' "2Recruiter", "recruiter2@gmail.com", "222-222-2222", 4);'),
+    ('INSERT INTO recruiters (first_name, last_name, email, phone, companyId) VALUES ("Recuiter3",'
+     ' "3Recruiter", "recruiter3@gmail.com", "333-333-3333", 1);'),
+    ('INSERT INTO recruiters (first_name, last_name, email, phone, companyId) VALUES ("Recuiter4",'
+     ' "4Recruiter", "recruiter4@gmail.com", "444-444-4444", 3);'),
+    ('INSERT INTO recruiters (first_name, last_name, email, phone, companyId) VALUES ("Recuiter5",'
+     ' "5Recruiter", "recruiter5@gmail.com", "555-555-5555", 2);'),
+    ('INSERT INTO recruiters (first_name, last_name, email, phone, companyId) VALUES ("Recuiter6",'
+     ' "6Recruiter", "recruiter6@gmail.com", "666-666-6666", 1);'),
+    ('INSERT INTO recruiters (first_name, last_name, email, phone, companyId) VALUES ("Recuiter7",'
+     ' "7Recruiter", "recruiter7@gmail.com", "777-777-7777", 1);')
+]
 
-JOB_POSTING_DATA = []
+JOB_POSTING_DATA = [
+    ('INSERT INTO job_posting (link, date_applied, description, interviewed, companyId,'
+     ' recruiterId, contactId) VALUES ("www.link.com", "DATE1", "Description",'
+     ' "yep", 1, 1, 1);')
+]
 
-# I need to move the test data insertion to python code
-"""
-# recruiters_data
-PRAGMA foreign_keys = ON;
-INSERT INTO recruiters (first_name, last_name, email, phone, companyId) VALUES ("Recuiter1",
-"1Recruiter", "recruiter1@gmail.com", "111-111-1111", 1);
-INSERT INTO recruiters (first_name, last_name, email, phone, companyId) VALUES ("Recuiter2",
-"2Recruiter", "recruiter2@gmail.com", "222-222-2222", 4);
-INSERT INTO recruiters (first_name, last_name, email, phone, companyId) VALUES ("Recuiter3",
-"3Recruiter", "recruiter3@gmail.com", "333-333-3333", 1);
-INSERT INTO recruiters (first_name, last_name, email, phone, companyId) VALUES ("Recuiter4",
-"4Recruiter", "recruiter4@gmail.com", "444-444-4444", 3);
-INSERT INTO recruiters (first_name, last_name, email, phone, companyId) VALUES ("Recuiter5",
-"5Recruiter", "recruiter5@gmail.com", "555-555-5555", 2);
-INSERT INTO recruiters (first_name, last_name, email, phone, companyId) VALUES ("Recuiter6",
-"6Recruiter", "recruiter6@gmail.com", "666-666-6666", 1);
-INSERT INTO recruiters (first_name, last_name, email, phone, companyId) VALUES ("Recuiter7",
-"7Recruiter", "recruiter7@gmail.com", "777-777-7777", 1);
-
-# job_posting_data
-PRAGMA foreign_keys = ON;
-INSERT INTO job_posting (link, date_applied, description, interviewed, companyId, recruiterId,
-contactId) VALUES ("www.link.com", "DATE1", "Description", "yep", 1, 1, 1);
-
-"""
+DB_TEST_DATA = [COMPANY_DATA, CONTACTS_DATA, RECRUITERS_DATA, JOB_POSTING_DATA]
