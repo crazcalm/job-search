3a5d83635ffb4b0beaa392aeda2204a7fbfbd431
Crazcalm <Crazcalm@gmail.com>
2015-09-16 00:52:46 -0400

    added sql queries to contact class

--- src/contact_class.py
+++ src/contact_class.py
@@ -4,6 +4,9 @@ except ImportError:
     from base_classes import SQLModule
 
 
+#Note: I need to separate Contact into two classes so that I do not pollute the
+# the Recruiter classes API...
+
 class Contact(SQLModule):
     def __init__(self, uid="", first_name="", last_name="", email="", phone="", description="",
                  company_uid=None):
@@ -20,6 +23,32 @@ class Contact(SQLModule):
     def full_name(self):
         return "{} {}".format(self.first_name, self.last_name)
 
+    def get_all_contacts(self):
+        if not self.db:
+            self.init_db()
+
+        query = """
+        SELECT id, first_name, last_name, email, phone, description, companyId
+        FROM contacts ORDER BY id;
+        """
+
+        data = self.db.con.execute(query)
+
+        return [Contact(*item) for item in data]
+
+    def get_a_contact(self, uid):
+        if not self.db:
+            self.init_db()
+
+        query = """
+        SELECT id, first_name, last_name, email, phone, description, companyId
+        FROM contacts WHERE (id=?) ORDER BY id;
+        """
+
+        data = self.db.con.execute(query, (uid,))
+
+        return [Contact(*item) for item in data]
+
     def __str__(self):
         return """
         uid: {}
@@ -35,3 +64,5 @@ class Contact(SQLModule):
 if __name__ == "__main__":
     test = Contact(first_name="Marcus")
     print(test)
+    test.get_all_contacts()
+    print(test.get_a_contact(1)[0])
