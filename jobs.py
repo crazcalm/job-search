6501c6fc153497156898cf78309fd157684aad95
crazcalm <crazcalm@gmail.com>
2015-09-22 14:26:06 -0400

    more shaping of what I want the cli to look like

--- jobs.py
+++ jobs.py
@@ -4,17 +4,33 @@ choices = ["company", "contact", "recruiter", "job_posting"]
 
 
 def main():
-    jobs= argparse.ArgumentParser(description="jobs", epilog="bye")
-    jobs.add_argument("--foo", action="store_true", help="foo help")
-    jobs.add_argument("-v, --verbose", action="store_true", help="verbose help")
+    jobs = argparse.ArgumentParser(description="jobs", epilog="bye")
+    jobs.add_argument("-v", action="store_true", help="verbose help")
     group = jobs.add_mutually_exclusive_group()
 
     group.add_argument("--add", choices=choices, help="add help")
     group.add_argument("--show", choices=choices, help="show help")
     group.add_argument("--update", choices=choices, help="update help")
 
-    test = jobs.parse_args()
-    print(test)
+    cli_args = jobs.parse_args()
+
+    if cli_args.add:
+        print("add was selected")
+        print("add: ", cli_args.add)
+
+    elif cli_args.show:
+        print("show was selected")
+        print("show:", cli_args.show)
+
+    elif cli_args.update:
+        print("update was selected")
+        print("update:", cli_args.update)
+
+    else:
+        print("nothing was selected")
+        print("I will print out the job postings")
+
+    print(cli_args)
 
 
 if __name__ == "__main__":
