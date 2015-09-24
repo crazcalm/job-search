55a230f24a4dcc1a12dc870e257c4ff17d9e6eb9
crazcalm <crazcalm@gmail.com>
2015-09-24 18:56:10 -0400

    I have some code that prints items to screen, but it needs to be cleaned up

--- jobs.py
+++ jobs.py
@@ -1,6 +1,49 @@
 import argparse
 
-choices = ["company", "contact", "recruiter", "job_posting"]
+from src.company_class import Company
+from src.contact_class import Contact
+from src.recruiter_class import Recruiter
+from src.job_posting_class import JobPosting
+
+JOB_POSTING = "job_posting"
+RECRUITER = "recruiter"
+CONTACT = "contact"
+COMPANY = "company"
+
+choices = [COMPANY, CONTACT, RECRUITER, JOB_POSTING]
+
+CLASSES = {
+    "company": {
+        "class": Company,
+        "get_all": "get_all_companies"
+    },
+    "contact": {
+        "class": Contact,
+        "get_all": "get_all_contacts"
+    },
+    "recruiter": {
+        "class": Recruiter,
+        "get_all": "get_all_recruiters"
+    },
+    "job_posting": {
+        "class": JobPosting,
+        "get_all": "get_all_job_postings"
+    }
+}
+
+
+def show(class_type):
+    result = None
+    if class_type in CLASSES:
+        class_info = CLASSES.get(class_type)
+        instance = class_info.get("class")()
+        result = getattr(instance, class_info.get("get_all"))()
+    return result
+
+
+def print_to_screen(items):
+    for index, item in enumerate(items):
+        print("{}: {}".format(index, item))
 
 
 def main():
@@ -21,6 +64,7 @@ def main():
     elif cli_args.show:
         print("show was selected")
         print("show:", cli_args.show)
+        print_to_screen(show(cli_args.show))
 
     elif cli_args.update:
         print("update was selected")
@@ -29,9 +73,11 @@ def main():
     else:
         print("nothing was selected")
         print("I will print out the job postings")
+        print_to_screen(show(JOB_POSTING))
 
     print(cli_args)
 
 
 if __name__ == "__main__":
     main()
+
