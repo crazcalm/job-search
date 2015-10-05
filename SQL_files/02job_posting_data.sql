4ad21d45e9071e6bb261be715afa6b769dbf3f8b
crazcalm <crazcalm@gmail.com>
2015-10-05 15:10:51 -0400

    I do not need these files anymore

--- SQL_files/02job_posting_data.sql
+++
@@ -1,3 +0,0 @@
-PRAGMA foreign_keys = ON;
-INSERT INTO job_posting (link, date_applied, description, interviewed, companyId, recruiterId,
-contactId) VALUES ("www.link.com", "DATE1", "Description", "yep", 1, 1, 1);
--- SQL_files/0company.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS company;
-CREATE TABLE company(
-    id          INTEGER PRIMARY KEY,
-    name        TEXT NOT NULL DEFAULT "",
-    address     TEXT NOT NULL DEFAULT "",
-    website     TEXT NOT NULL DEFAULT "",
-    phone       TEXT NOT NULL DEFAULT ""
-);
