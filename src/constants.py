66da014df2a1e79703f562204ff83967e4c37879
crazcalm <crazcalm@gmail.com>
2015-10-06 08:18:53 -0400

    create a constant for the path to the real_db. I still need to create a function to ensure that the db is created and I also need to change the dir name of sql_files to something more proper

--- src/constants.py
+++ src/constants.py
@@ -2,6 +2,7 @@ import os
 
 # test database
 TEST_DB = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "SQL_files", "test.db"))
+REAL_DB = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "SQL_files", "real.db"))
 SQL_FILES = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "SQL_files"))
 
 
