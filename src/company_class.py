6bbf6a194c105e0d8553340e31f00e420c189c3c
crazcalm <crazcalm@gmail.com>
2015-10-09 05:45:22 +0800

    added documentation

--- src/company_class.py
+++ src/company_class.py
@@ -7,12 +7,32 @@ except ImportError:
 
 
 class Company(SQLModule):
+    """
+    This class is dedicated to modeling a Company.
+
+    Class properties include:
+    - table_name: str - db table name
+    - columns: tuple - column names for table.
+    - columns_with_uid: tuple - column names for table (including the id)
+    """
     _columns = ["id", "name", "address", "website", "phone"]
     table_name = "company"
     columns = tuple(_columns[1:])
     columns_with_uid = tuple(_columns)
 
     def __init__(self, uid="", name="", address="", website="", phone="", testing=False):
+        """
+        Initialization of class.
+
+        :param uid: int or empty string - db unique identifier
+        :param name: str
+        :param address: str
+        :param website: str
+        :param phone: str
+        :param testing: boolean
+        """
+
+        # Initialization of SQLModule
         super(Company, self).__init__()
         self.uid = uid
         self.name = name
@@ -23,21 +43,38 @@ class Company(SQLModule):
 
     @property
     def properties(self):
+        """
+        Returns a tuple of the class's property names.
+        """
         return self.properties_with_uid[1:]
 
     @property
     def properties_with_uid(self):
+        """
+        Returns a tuple of the class's property names (including the uid).
+        """
         return "uid", "name", "address", "website", "phone"
 
     @property
     def values(self):
+        """
+        Returns a tuple of the values of the class's properties. The order
+        is (name, address, website, phone)
+        """
         return self.name, self.address, self.website, self.phone
 
     @property
     def values_with_id(self):
+        """
+        Returns a tuple of the values of the class's properties (including uid).
+        The order is (name, address, website, phone, uid)
+        """
         return self.name, self.address, self.website, self.phone, self.uid
 
     def get_all_companies(self):
+        """
+        This method returns a list of all of the Companies in the database.
+        """
         self.init_db(self._testing)
 
         query = "SELECT {} FROM {} ORDER BY id;".format(", ".join(Company.columns_with_uid), Company.table_name)
@@ -47,6 +84,11 @@ class Company(SQLModule):
         return [Company(*item) for item in data]
 
     def get_a_company(self, uid):
+        """
+        This method returns a single Company from the database.
+
+        :param uid: int - unique identifier of a company instance
+        """
         assert isinstance(uid, int)
         self.init_db(self._testing)
 
@@ -58,6 +100,9 @@ class Company(SQLModule):
         return [Company(*item) for item in data]
 
     def add_company_to_db(self):
+        """
+        This method adds this company instance to the database.
+        """
         self.init_db(self._testing)
 
         # Make sure that the Company does not already exist
@@ -69,6 +114,9 @@ class Company(SQLModule):
         self.uid = self._get_id_of_last_row(Company.table_name)
 
     def update_company_in_db(self):
+        """
+        This method updates this company instance in the database.
+        """
         self.init_db(self._testing)
 
         # make sure tht the Company does exist in the
@@ -77,6 +125,9 @@ class Company(SQLModule):
         self._update_row_in_db(Company.table_name, Company.columns, self.values_with_id)
 
     def delete_company_in_db(self):
+        """
+        This method deletes this company instance from the database.
+        """
         self.init_db(self._testing)
 
         # Make sure that the Company exists
@@ -92,5 +143,5 @@ class Company(SQLModule):
         phone: {}""".format(self.name, self.address, self.website, self.phone)
 
 if __name__ == "__main__":
-    test = Company(name="DavidInc")
-    print(test.columns)
+    pass
+
